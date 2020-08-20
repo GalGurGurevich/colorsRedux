9832e81401bb33d3ce5714d8614e4a681aae6ce2
Gal Gur <gal.gurevich@gmail.com>
2020-08-20 22:02:26 +0300

    fix bug to display mixed color background, added css and colorBoard logic

--- src/components/colorBoard/colorBoard.js
+++ src/components/colorBoard/colorBoard.js
@@ -6,8 +6,19 @@ import { connect } from 'react-redux'
 
 function colorBoard(props) {
 
+    const first = props.colorFirst.color;
+    const second = props.colorSecond.color;
+    let output = "";
+
+    if(first === "yellow" && second === "red") output = "orange";
+    if(first === "red" && second === "yellow") output = "orange";
+    if(first === "blue" && second === "red") output = "purple";
+    if(first === "red" && second === "blue") output = "purple";
+    if(first === "blue" && second === "yellow") output = "green";
+    if(first === "yellow" && second === "blue") output = "green";
+
     return( 
-        <div className="canvasBackground">
+        <div className={'canvasBackground' + (output)}>
             <div id="welcome">WELCOME GAL WORLD</div>
             <button onClick={() => props.mixColors()}>PRESS TO MIX COLORS</button>
             <button onClick={() => props.removeColor()}>PRESS TO REMOVE COLOR</button>
@@ -16,10 +27,10 @@ function colorBoard(props) {
                 <div id="red" onClick={() => props.addColor("red")}>ADD RED</div>
                 <div id="blue" onClick={() => props.addColor("blue")}>ADD BLUE</div>
             </div>
-            <div>YOU CHOSE: {props.colorFirst}</div>
-            <div>MIX WITH: {props.colorSecond}</div>
-            <div>YOUR GET COLOR: </div>
-            <div>HISTORY MOVES: {props.history}</div>
+            <div>YOU CHOSE: {props.colorFirst.color}</div>
+            <div>MIX WITH: {props.colorSecond.color}</div>
+            <div>YOUR GET COLOR: {output}</div>
+            <div>HISTORY MOVES: {props.history.map((color, idx) => (<div key={idx}>{color.color}</div>))}</div>
         </div>
     )
 };
@@ -27,7 +38,8 @@ function colorBoard(props) {
 const mapStateToProps = (state) => ({
     colorFirst: state.mixColors[0] || '',
     colorSecond: state.mixColors[1] || '',
-    mixColor: state.history[state.history.length - 1]
+    mixColor: state.history[state.history.length - 1] || [],
+    history: state.history
   })
 
 const mapDispatchToProps = dispatch => ({
